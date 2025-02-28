83e761c8d80fb6f286c66914b07a5089c4f907cb
Lavkush Patel <166002653+Lavkush-patel9@users.noreply.github.com>
2025-02-28 12:10:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("script Running...")
+
+document.querySelector('.cross').style.display='none';
+document.querySelector('.hamburger').addEventListener("click", () =>{
+    document.querySelector('.side-bar').classList.toggle('side-barGo')
+    if(document.querySelector('.side-bar').classList.contains('side-barGo')){
+        document.querySelector('.ham').style.display='inline'
+        document.querySelector('.cross').style.display='none'
+    }
+    else{  
+        document.querySelector('.ham').style.display='none'
+        setTimeout(() => {
+            document.querySelector('.cross').style.display='inline'
+        },300);
+
+    }
+});
